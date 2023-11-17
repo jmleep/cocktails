04c4665798c7ac737dd68a71845e4f78d1014978
Jordan Leeper <j.m.leep@gmail.com>
2023-11-16 21:52:45 -0600

    started formatting

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
 import { CocktailService } from '../cocktail.service';
 
 @Component({
@@ -12,32 +13,61 @@ import { CocktailService } from '../cocktail.service';
     MatSelectModule,
     MatFormFieldModule,
     NgOptimizedImage,
+    MatCardModule,
   ],
   styleUrl: './landing.component.scss',
-  template: ` <div>
-    <mat-form-field>
-      <mat-label>Select ingredient</mat-label>
-      <mat-select
-        [value]="cocktailService.alcohol()"
-        (valueChange)="cocktailService.alcohol.set($event)"
-      >
-        @for (food of [{value: 'gin', viewValue: 'Gin'}, {value: 'vodka',
-        viewValue: 'Vodka'}]; track food) {
-        <mat-option [value]="food.value">{{ food.viewValue }}</mat-option>
-        }
-      </mat-select></mat-form-field
-    >
-
-    @if (cocktailService.drinksList().length) {
+  template: ` <h1>Cocktail Suggestor</h1>
     <div>
-      @for (cocktail of cocktailService.drinksList(); track cocktail.idDrink) {
-      <p>{{ cocktail.strDrink }}</p>
-      <img [ngSrc]="cocktail.strDrinkThumb" height="200" width="200" />
+      <mat-form-field>
+        <mat-label>Select alcohol</mat-label>
+        <mat-select
+          [value]="cocktailService.alcohol()"
+          (valueChange)="cocktailService.alcohol.set($event)"
+        >
+          @for (alcohol of alcoholList; track alcohol) {
+          <mat-option [value]="alcohol">{{ alcohol }}</mat-option>
+          }
+        </mat-select></mat-form-field
+      >
+
+      @if (cocktailService.drinksList().length) {
+      <div>
+        @for (cocktail of cocktailService.drinksList(); track cocktail.idDrink)
+        {
+        <mat-card>
+          <mat-card-header
+            ><mat-card-title>{{
+              cocktail.strDrink
+            }}</mat-card-title></mat-card-header
+          >
+          <img
+            mat-card-image
+            [ngSrc]="cocktail.strDrinkThumb"
+            height="200"
+            width="200"
+          />
+        </mat-card>
+        }
+      </div>
       }
-    </div>
-    }
-  </div>`,
+    </div>`,
 })
 export class LandingComponent {
   cocktailService = inject(CocktailService);
+
+  alcoholList = [
+    'Absinthe',
+    'Amaretto',
+    'Bourbon',
+    'Brandy',
+    'Cognac',
+    'Gin',
+    'Jägermeister',
+    'Rum',
+    'Sake',
+    'Scotch',
+    'Tequila',
+    'Vodka',
+    'Whiskey',
+  ];
 }
