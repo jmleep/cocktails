f2bdf77e3b6806d2edbb525e82f53a09abf63d2a
Jordan Leeper <j.m.leep@gmail.com>
2024-02-24 14:12:17 -0600

    added sharing and back button, fixes image sizing, fixes misaligned column in grid

--- src/app/pages/cocktail-detail-page/cocktail-detail-page.component.ts
+++ src/app/pages/cocktail-detail-page/cocktail-detail-page.component.ts
@@ -5,6 +5,9 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RouterModule } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'cocktail-detail-page',
@@ -16,6 +19,7 @@ import { MatIconModule } from '@angular/material/icon';
     RouterModule,
     MatListModule,
     MatIconModule,
+    MatButtonModule,
   ],
   templateUrl: './cocktail-detail-page.component.html',
   styleUrl: './cocktail-detail-page.component.scss',
@@ -24,8 +28,29 @@ export class CocktailDetailPageComponent implements OnInit {
   @Input() id!: string;
   cocktailSerivce = inject(CocktailService);
   cocktail: ICocktail | undefined;
+  snackBar = inject(MatSnackBar);
+  location = inject(Location);
 
   async ngOnInit(): Promise<void> {
     this.cocktail = await this.cocktailSerivce.getById(this.id);
+  }
+
+  back() {
+    this.location.back();
+  }
+
+  share(cocktail: ICocktail) {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: cocktail.strDrink,
+          text: 'Check out this drink from Cocktail Genie!',
+          url: window.location.href,
+        })
+        .catch((error) => console.log('Error sharing', error));
+    } else {
+      navigator.clipboard.writeText(window.location.href);
+      this.snackBar.open('Link copied', '', { duration: 2000 });
+    }
   }
 }
